bbe352d7066ffeda1364cebc4709ebc5b8c73f11
Vladimir Osipchuk <vladimir.osipchuk@duallab.com>
2017-03-31 15:20:32 +0300

    Port new sandbox tests based on SO answers

---
+++ src/test/java/com/itextpdf/samples/sandbox/tables/ImageRowspan.java
@@ -0,0 +1,54 @@
+/*
+    This file is part of the iText (R) project.
+    Copyright (c) 1998-2017 iText Group NV
+    Authors: iText Software.
+
+    For more information, please contact iText Software at this address:
+    sales@itextpdf.com
+ */
+/**
+ * Example written in answer to:
+ * http://stackoverflow.com/questions/42440133/itext-cell-with-image-doesnt-apply-rowspan
+ */
+
+package com.itextpdf.samples.sandbox.tables;
+
+import com.itextpdf.io.image.ImageDataFactory;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.Image;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.samples.GenericTest;
+import com.itextpdf.test.annotations.type.SampleTest;
+import org.junit.experimental.categories.Category;
+
+import java.io.File;
+
+@Category(SampleTest.class)
+public class ImageRowspan extends GenericTest {
+    public static final String DEST = "./target/test/resources/sandbox/tables/image_rowspan.pdf";
+    public static final String IMG = "./src/test/resources/img/bruno.jpg";
+
+
+    public static void main(String[] args) throws Exception {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new ImageRowspan().manipulatePdf(DEST);
+    }
+
+    @Override
+    protected void manipulatePdf(String dest) throws Exception {
+        PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
+        Document doc = new Document(pdfDoc);
+        Table table = new Table(2);
+
+        table.addCell(new Cell(2, 1).add(new Image(ImageDataFactory.create(IMG)).setWidthPercent(100)));
+        table.addCell("1");
+        table.addCell("2");
+        doc.add(table);
+
+        doc.close();
+    }
+}
---
+++ src/test/java/com/itextpdf/samples/sandbox/tables/TableWithSeparator.java
@@ -0,0 +1,106 @@
+/*
+    This file is part of the iText (R) project.
+    Copyright (c) 1998-2017 iText Group NV
+    Authors: iText Software.
+
+    For more information, please contact iText Software at this address:
+    sales@itextpdf.com
+ */
+/**
+ * Example written by Bruno Lowagie in answer to the following question:
+ * http://stackoverflow.com/questions/40607960
+ */
+
+package com.itextpdf.samples.sandbox.tables;
+
+import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import com.itextpdf.kernel.pdf.canvas.draw.SolidLine;
+import com.itextpdf.kernel.pdf.xobject.PdfFormXObject;
+import com.itextpdf.layout.Canvas;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.LineSeparator;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.samples.GenericTest;
+import com.itextpdf.test.annotations.type.SampleTest;
+
+import org.junit.experimental.categories.Category;
+
+import java.io.File;
+
+@Category(SampleTest.class)
+public class TableWithSeparator extends GenericTest {
+    public static final String DEST = "./target/test/resources/sandbox/tables/table_with_separator.pdf";
+
+    public static void main(String[] args) throws Exception {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new TableWithSeparator().manipulatePdf(DEST);
+    }
+
+    @Override
+    protected void manipulatePdf(String dest) throws Exception {
+        PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
+        Document doc = new Document(pdfDoc);
+
+        Table table = new Table(1);
+        table.addCell(getCell1());
+        table.addCell(getCell2());
+        table.addCell(getCell3());
+        table.addCell(getCell4());
+        doc.add(table);
+
+        doc.close();
+    }
+
+    public Cell getCell1() {
+        Cell cell = new Cell();
+        Paragraph p1 = new Paragraph("My fantastic data");
+        p1.setLineThrough();
+        cell.add(p1);
+        Paragraph p2 = new Paragraph("Other data");
+        cell.add(p2);
+        return cell;
+    }
+
+    public Cell getCell2() {
+        Cell cell = new Cell();
+        Paragraph p1 = new Paragraph("My fantastic data");
+        cell.add(p1);
+        LineSeparator ls = new LineSeparator(new SolidLine());
+        cell.add(ls);
+        Paragraph p2 = new Paragraph("Other data");
+        cell.add(p2);
+        return cell;
+    }
+
+    public Cell getCell3() {
+        Cell cell = new Cell();
+        Paragraph p1 = new Paragraph("My fantastic data");
+        cell.add(p1);
+        LineSeparator ls = new LineSeparator(new SolidLine());
+        cell.add(ls);
+        Paragraph p2 = new Paragraph("Other data");
+        p2.setFixedLeading(25);
+        cell.add(p2);
+        return cell;
+    }
+
+    public Cell getCell4() {
+        Cell cell = new Cell();
+        Paragraph p1 = new Paragraph("My fantastic data");
+        p1.setMarginBottom(20);
+        cell.add(p1);
+        LineSeparator ls = new LineSeparator(new SolidLine());
+        cell.add(ls);
+        Paragraph p2 = new Paragraph("Other data");
+        p2.setMarginTop(10);
+        cell.add(p2);
+        return cell;
+    }
+
+}
