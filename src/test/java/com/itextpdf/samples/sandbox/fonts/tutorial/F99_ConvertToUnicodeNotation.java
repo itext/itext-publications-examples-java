4906d91ccc47ed7703f9027adc852dea655d37c2
Pavel Alay <pavel.alay@duallab.com>
2019-07-04 23:32:14 +0300

    Revert unicode string

--- src/test/java/com/itextpdf/samples/sandbox/fonts/tutorial/F99_ConvertToUnicodeNotation.java
+++ src/test/java/com/itextpdf/samples/sandbox/fonts/tutorial/F99_ConvertToUnicodeNotation.java
@@ -13,7 +13,7 @@ package com.itextpdf.samples.sandbox.fonts.tutorial;
 
 public class F99_ConvertToUnicodeNotation {
     public static void main() throws Exception {
-        String s = "Vous ?tes d'o??";
+        String s = "Vous êtes d'où?";
         System.out.print("\"");
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
