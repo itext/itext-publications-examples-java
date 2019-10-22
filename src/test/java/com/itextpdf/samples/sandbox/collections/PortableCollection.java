bbb69891bcb6e09452abeea76f01977a5fe9c33c
Yauheni Borbut <yauheni.borbut@duallab.com>
2019-10-22 15:06:18 +0300

    Update Collections samples
    DEVSIX-3392

--- src/test/java/com/itextpdf/samples/sandbox/collections/PortableCollection.java
+++ src/test/java/com/itextpdf/samples/sandbox/collections/PortableCollection.java
@@ -20,39 +20,49 @@ import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
 
 import java.io.File;
+import java.io.IOException;
 
 public class PortableCollection {
-    public static final String DATA
-            = "./src/test/resources/data/united_states.csv";
-    public static final String DEST
-            = "./target/sandbox/collections/portable_collection.pdf";
-    public static final String HELLO
-            = "./src/test/resources/pdfs/hello.pdf";
-    public static final String IMG =
-            "./src/test/resources/img/berlin2013.jpg";
+    public static final String DEST = "./target/sandbox/collections/portable_collection.pdf";
+
+    public static final String DATA = "./src/test/resources/data/united_states.csv";
+    public static final String HELLO = "./src/test/resources/pdfs/hello.pdf";
+    public static final String IMG = "./src/test/resources/img/berlin2013.jpg";
 
     public static void main(String[] args) throws Exception {
         File file = new File(DEST);
         file.getParentFile().mkdirs();
+
         new PortableCollection().manipulatePdf(DEST);
     }
 
     protected void manipulatePdf(String dest) throws Exception {
-        PdfDocument pdfDoc = new PdfDocument(new PdfWriter(DEST));
+        PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
         Document doc = new Document(pdfDoc);
+
         doc.add(new Paragraph("Portable collection"));
+        
         PdfCollection collection = new PdfCollection();
         collection.setView(PdfCollection.TILE);
         pdfDoc.getCatalog().setCollection(collection);
-        pdfDoc.addFileAttachment("united_states.csv",
-                PdfFileSpec.createEmbeddedFileSpec(pdfDoc, DATA, "united_states.csv", "united_states.csv",
-                        null, null));
-        pdfDoc.addFileAttachment("hello.pdf",
-                PdfFileSpec.createEmbeddedFileSpec(pdfDoc, HELLO, "hello.pdf", "hello.pdf",
-                        null, null));
-        pdfDoc.addFileAttachment("berlin2013.jpg",
-                PdfFileSpec.createEmbeddedFileSpec(pdfDoc, IMG, "berlin2013.jpg", "berlin2013.jpg",
-                        null, null));
+
+        addFileAttachment(pdfDoc, DATA, "united_states.csv");
+        addFileAttachment(pdfDoc, HELLO, "hello.pdf");
+        addFileAttachment(pdfDoc, IMG, "berlin2013.jpg");
+
         doc.close();
+    }
+
+    // This method adds file attachment to the pdf document
+    private void addFileAttachment(PdfDocument document, String attachmentPath, String fileName) throws IOException {
+        String embeddedFileName = fileName;
+        String embeddedFileDescription = fileName;
+        String fileAttachmentKey = fileName;
+
+        // the 5th argument is the mime-type of the embedded file;
+        // the 6th argument is the AFRelationship key value.
+        PdfFileSpec fileSpec = PdfFileSpec.createEmbeddedFileSpec(document, attachmentPath, embeddedFileDescription,
+                embeddedFileName, null, null);
+        document.addFileAttachment(fileAttachmentKey, fileSpec);
     }
 }
