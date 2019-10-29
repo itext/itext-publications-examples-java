b733edbe060cd5c943a3fce06a4c4a72b11d24b4
Alex Rasolko <alex.rasolko@duallab.com>
2019-10-29 13:54:59 +0300

    Refactor MathSymbols sample. DEVSIX-3127

--- src/test/java/com/itextpdf/samples/sandbox/fonts/MathSymbols.java
+++ src/test/java/com/itextpdf/samples/sandbox/fonts/MathSymbols.java
@@ -33,17 +33,14 @@ public class MathSymbols {
     }
 
     protected void manipulatePdf(String dest) throws Exception {
-        PdfDocument pdfDoc = new PdfDocument(new PdfWriter(DEST));
+        PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
         Document doc = new Document(pdfDoc);
 
-        PdfFont f1 = PdfFontFactory.createFont(FONT, PdfEncodings.CP1250, true);
-        Paragraph p1 = new Paragraph("Testing of letters \u010c,\u0106,\u0160,\u017d,\u0110").setFont(f1);
-        doc.add(p1);
+        PdfFont font = PdfFontFactory.createFont(FONT, PdfEncodings.IDENTITY_H, true);
 
-        PdfFont f2 = PdfFontFactory.createFont(FONT, PdfEncodings.IDENTITY_H);
-        Paragraph p2 = new Paragraph("Testing of letters \u010c,\u0106,\u0160,\u017d,\u0110").setFont(f2);
-        doc.add(p2);
+        Paragraph p = new Paragraph("Testing math symbols \u2208, \u2229, \u2211, \u222b, \u2206").setFont(font);
 
+        doc.add(p);
         doc.close();
     }
 }
