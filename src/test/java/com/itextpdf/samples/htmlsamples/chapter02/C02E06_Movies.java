efc2d7da55dd528cb4ddc67c8787e383d68438ec
Yauheni Borbut <yauheni.borbut@duallab.com>
2019-12-26 14:30:47 +0300

    Update the chapter02 of html samples
    DEVSIX-3508

--- src/test/java/com/itextpdf/samples/htmlsamples/chapter02/C02E06_Movies.java
+++ src/test/java/com/itextpdf/samples/htmlsamples/chapter02/C02E06_Movies.java
@@ -1,7 +1,13 @@
 /*
- * Copyright 2016-2017, iText Group NV.
- * This example was created by Bruno Lowagie.
- * It was written in the context of the following book:
+    This file is part of the iText (R) project.
+    Copyright (c) 1998-2019 iText Group NV
+    Authors: iText Software.
+
+    For more information, please contact iText Software at this address:
+    sales@itextpdf.com
+ */
+/*
+ * This example was written in the context of the following book:
  * https://leanpub.com/itext7_pdfHTML
  * Go to http://developers.itextpdf.com for more info.
  */
@@ -18,25 +24,27 @@ import com.itextpdf.licensekey.LicenseKey;
  */
 public class C02E06_Movies {
 
-	/** The Base URI of the HTML page. */
-	public static final String BASEURI = "src/main/resources/html/";
-	/** The path to the source HTML file. */
-	public static final String SRC = String.format("%smovies.html", BASEURI);
-	/** The target folder for the result. */
-	public static final String TARGET = "target/results/ch02/";
-	/** The path to the resulting PDF file. */
-	public static final String DEST = String.format("%smovie06.pdf", TARGET);
+    /**
+     * The path to the resulting PDF file.
+     */
+    public static final String DEST = "./target/htmlsamples/ch02/movie06.pdf";
 
-	/**
-	 * The main method of this example.
-	 *
-	 * @param args no arguments are needed to run this example.
-	 * @throws IOException Signals that an I/O exception has occurred.
-	 */
+    /**
+     * The path to the source HTML file.
+     */
+    public static final String SRC = "./src/test/resources/htmlsamples/html/movies.html";
+
+    /**
+     * The main method of this example.
+     *
+     * @param args no arguments are needed to run this example.
+     * @throws IOException signals that an I/O exception has occurred.
+     */
     public static void main(String[] args) throws IOException {
         LicenseKey.loadLicenseFile(System.getenv("ITEXT7_LICENSEKEY") + "/itextkey-html2pdf_typography.xml");
-    	File file = new File(TARGET);
-    	file.mkdirs();
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+
         C02E06_Movies app = new C02E06_Movies();
         app.createPdf(SRC, DEST);
     }
@@ -44,11 +52,11 @@ public class C02E06_Movies {
     /**
      * Creates the PDF file.
      *
-     * @param src the path to the source HTML file
+     * @param src  the path to the source HTML file
      * @param dest the path to the resulting PDF
-     * @throws IOException Signals that an I/O exception has occurred.
+     * @throws IOException signals that an I/O exception has occurred.
      */
     public void createPdf(String src, String dest) throws IOException {
-		HtmlConverter.convertToPdf(new File(src), new File(dest));
+        HtmlConverter.convertToPdf(new File(src), new File(dest));
     }
 }
