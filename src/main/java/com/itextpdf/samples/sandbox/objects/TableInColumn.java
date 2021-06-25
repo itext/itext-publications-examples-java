04fcdde2512be23ff0277f0312cafb7be7cc439d
Nikita Kovaliov <nikita.kovaliov@duallab.com>
2021-06-25 11:57:36 +0300

    Update comment about the importance of getNextRenderer
    DEVSIX-5537

--- src/main/java/com/itextpdf/samples/sandbox/objects/TableInColumn.java
+++ src/main/java/com/itextpdf/samples/sandbox/objects/TableInColumn.java
@@ -75,8 +75,8 @@ public class TableInColumn {
             this.rect = rect;
         }
 
-        // If renderer overflows on the next area, iText uses getNextRender() method to create a renderer for the overflow part.
-        // If getNextRenderer isn't overridden, the default method will be used and thus a default rather than custom
+        // If a renderer overflows on the next area, iText uses #getNextRenderer() method to create a new renderer for the overflow part.
+        // If #getNextRenderer() isn't overridden, the default method will be used and thus the default rather than the custom
         // renderer will be created
         @Override
         public IRenderer getNextRenderer() {
