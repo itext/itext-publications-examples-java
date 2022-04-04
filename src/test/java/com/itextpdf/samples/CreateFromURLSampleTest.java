c4e3e797397dad49166cb01add67b5b82eace378
Alexander Kozlov <alexander.kozlov@duallab.com>
2022-04-04 17:29:59 +0300

    Fix keywords set for SO test
    (cherry picked from commit 03881dec6eebcd1626a5deb833878e27ad547908)

--- src/test/java/com/itextpdf/samples/CreateFromURLSampleTest.java
+++ src/test/java/com/itextpdf/samples/CreateFromURLSampleTest.java
@@ -47,7 +47,7 @@ public class CreateFromURLSampleTest extends WrappedSamplesRunner {
 
         expectedNumbersOfPages.put("com.itextpdf.samples.htmlsamples.chapter07.C07E04_CreateFromURL", new Integer[] {2, 4});
         expectedNumbersOfPages.put("com.itextpdf.samples.htmlsamples.chapter07.C07E05_CreateFromURL2", new Integer[] {2, 4});
-        expectedNumbersOfPages.put("com.itextpdf.samples.htmlsamples.chapter07.C07E06_CreateFromURL3", new Integer[] {3, 4});
+        expectedNumbersOfPages.put("com.itextpdf.samples.htmlsamples.chapter07.C07E06_CreateFromURL3", new Integer[] {3, 4, 5});
     }
 
     @Parameterized.Parameters(name = "{index}: {0}")
