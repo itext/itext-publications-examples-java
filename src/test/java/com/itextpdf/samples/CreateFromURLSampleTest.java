1f956fb3101c0f429eddae8d215bc496225e1038
Angelina Pavlovets <angelina.pavlovets@duallab.com>
2022-04-24 12:04:40 +0300

    Fix test for com.itextpdf.samples.htmlsamples.chapter07.C07E06_CreateFromURL3 sample
    DEVSIX-6547

--- src/test/java/com/itextpdf/samples/CreateFromURLSampleTest.java
+++ src/test/java/com/itextpdf/samples/CreateFromURLSampleTest.java
@@ -45,7 +45,7 @@ public class CreateFromURLSampleTest extends WrappedSamplesRunner {
 
         expectedNumbersOfPages.put("com.itextpdf.samples.htmlsamples.chapter07.C07E04_CreateFromURL", new Integer[] {2, 4});
         expectedNumbersOfPages.put("com.itextpdf.samples.htmlsamples.chapter07.C07E05_CreateFromURL2", new Integer[] {3, 4});
-        expectedNumbersOfPages.put("com.itextpdf.samples.htmlsamples.chapter07.C07E06_CreateFromURL3", new Integer[] {3, 4, 5});
+        expectedNumbersOfPages.put("com.itextpdf.samples.htmlsamples.chapter07.C07E06_CreateFromURL3", new Integer[] {2});
     }
 
     @Parameterized.Parameters(name = "{index}: {0}")
