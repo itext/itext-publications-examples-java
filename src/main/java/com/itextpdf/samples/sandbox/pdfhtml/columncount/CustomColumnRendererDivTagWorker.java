80aa76234b2013d468e34c81fe189902ca1fea63
Guust Ysebie <guust.ysebie@apryse.com>
2024-01-15 15:31:02 +0100

    Update copyright notice 2024
    DEVSIX-7291

--- src/main/java/com/itextpdf/samples/sandbox/pdfhtml/columncount/CustomColumnRendererDivTagWorker.java
+++ src/main/java/com/itextpdf/samples/sandbox/pdfhtml/columncount/CustomColumnRendererDivTagWorker.java
@@ -1,3 +1,11 @@
+/*
+    This file is part of the iText (R) project.
+    Copyright (c) 1998-2024 Apryse Group NV
+    Authors: Apryse Software.
+
+    For more information, please contact iText Software at this address:
+    sales@itextpdf.com
+ */
 package com.itextpdf.samples.sandbox.pdfhtml.columncount;
 
 import com.itextpdf.html2pdf.ConverterProperties;
