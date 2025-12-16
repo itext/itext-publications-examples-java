25cfabc0de7ef040a8ad21928f9ab9d23f539a23
Angelina Pavlovets <angelina.pavlovets@apryse.com>
2025-12-16 21:43:59 +0300

    Temporarily disable tests that fail on Windows Server 2025 and Windows 11
    DEVSIX-9261

--- src/test/java/com/itextpdf/samples/GenericSampleTest.java
+++ src/test/java/com/itextpdf/samples/GenericSampleTest.java
@@ -152,9 +152,13 @@ public class GenericSampleTest extends WrappedSamplesRunner {
         // TODO DEVSIX-3326
         searchConfig.ignorePackageOrClass("com.itextpdf.samples.sandbox.tables.SplittingNestedTable2");
 
-        //TODO DEVSIX-6508 remove unnecessary makeFormField calls
+        // TODO DEVSIX-6508 remove unnecessary makeFormField calls
         searchConfig.ignorePackageOrClass("com.itextpdf.samples.sandbox.acroforms.RemoveXFA");
 
+        // TODO DEVSIX-9261 Investigate test failures on Windows Server 2025 and Windows 11
+        searchConfig.ignorePackageOrClass("com.itextpdf.samples.sandbox.images.ReplaceImage");
+        searchConfig.ignorePackageOrClass("com.itextpdf.samples.sandbox.images.MakeJpgMask");
+        searchConfig.ignorePackageOrClass("com.itextpdf.samples.sandbox.images.ReduceSize");
 
         return generateTestsList(searchConfig);
     }
